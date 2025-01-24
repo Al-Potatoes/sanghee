69573011eb9eb420943649c3de0e7c17cd9a5a5c
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-01-25 03:35:15 +0900

    [2주차] Q.2014 (힙)

---
+++ src/heap/Q2014.java
@@ -0,0 +1,41 @@
+package heap;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Q2014 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] line = br.readLine().split(" ");
+        int k = Integer.parseInt(line[0]);
+        int n = Integer.parseInt(line[1]);
+
+        long[] arr = new long[k];
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < k; i++) {
+            arr[i] = Long.parseLong((st.nextToken()));
+            pq.add(arr[i]);
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            long num = pq.poll();
+
+            for (int j = 0; j < k; j++) {
+                pq.add(num * arr[j]);
+
+                if (num % arr[j] == 0) {
+                    break;
+                }
+            }
+        }
+
+        System.out.println(pq.poll());
+    }
+}
