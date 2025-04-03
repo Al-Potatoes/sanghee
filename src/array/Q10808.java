4dfb9a4c159e8d2fa2c997d8e90ed389041e2f35
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-04-04 00:43:52 +0900

    [9주차] Q.10808 (문자열)

---
+++ src/array/Q10808.java
@@ -0,0 +1,25 @@
+package array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q10808 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int[] arr = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            arr[c - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
